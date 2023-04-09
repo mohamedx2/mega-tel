c57dfaa95c0b050c8165c8ee60aea88a377ebaa4
mohamedx2 <hamroun1969@gmail.com>
2023-04-09 03:50:58 +0100

    first commit

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,6 +1,22 @@
 const User = require('../models/user.model');
 const Article = require('../models/article.model');
-let controller={}
+let controller = {}
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+controller.verify= async (req,res,next)=>{
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) return res.status(401).send({ message: 'Unauthorized' });
+  try {
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+    req.decodedToken=decodedToken
+    next()
+  } catch (error) {
+    return res.send({message:'Unauthorized from verify'})
+  }
+}
+
+
 controller.signUp = async (req, res) => {
   try {
     // Check if user already exists
@@ -8,82 +24,149 @@ controller.signUp = async (req, res) => {
 
     // If user already signed up, don't allow them to make another account
     if (existingUser) {
-      return res.send({message:"already signed up",success:false});
+      return res.send({ message: "already signed up", success: false });
     }
 
-    // If not, create a new user and save
+    // Hash the password with bcrypt
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // Replace the plain text password with the hashed one
+    req.body.password = hashedPassword;
+
+    // Create a new user and save
     const newUser = new User(req.body);
 
     // Save the new user to the database
     const savedUser = await newUser.save();
 
-    // Return success response with the saved user
+    // Generate JWT token for the user
+    const token = jwt.sign(
+      { userId: savedUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    // Return success response with the saved user and token
     return res.send({
       success: true,
       message: "Created new user",
-      data: savedUser,
+      data: {
+        user: savedUser,
+        token: token,
+      },
     });
   } catch (error) {
     console.error(error);
     return res.status(500).send("An error occurred while signing up.");
   }
 };
-controller.update=(req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
-    if (err) {
-      res.send({err,success:false});
-    }else{
-      res.json({updatedUser,message:"user updated succesfulay",success:true});
-    }
-  });
-}
+
+controller.update = async (req, res) => {
+  try {
+    req.body.password=await bcrypt.hash(req.body.password, 10)
+    const  updatedUser= await User.findByIdAndUpdate(
+      req.decodedToken.userId,
+      req.body,
+      { new: true },
+    );
+    res.send({ updatedUser, message:"user updated with succes", success:true });
+  } catch (error) {
+    return res.send({
+      message: "An error occurred while updating user.",
+    });
+  }
+};
 controller.login = async (req, res) => {
   try {
-      // Access the credentials passed up in the request body
-      const { email, password } = req.body; 
-      
-      // Attempt to find the user based on the provided email
-      const theUser = await User.findOne({email});
-      
-      // Check if the password matches the one stored in the database
-      if(password == theUser.password) {
-          // If the passwords match then we send the user 
-          res.status(200).send(theUser);
-      }
-      else {
-          // Otherwise, we respond with an invalid login message
-          res.status(400).send('Invalid Login!');
-      }
+    // Check if user exists
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    // If user doesn't exist, return error response
+    if (!existingUser) {
+      return res.send({ message: "user not found", success: false });
+    }
+    // Compare the password with bcrypt
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, existingUser.password);
+    // If password is incorrect, return error response
+    if (!isPasswordCorrect) {
+      return res.send({ message: 'invalid credentials', success: false });
+    }
+
+    // Generate JWT token for the user
+    const token = jwt.sign(
+      { userId: existingUser._id},
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    // Return success response with the generated token
+    return res.send({
+      success: true,
+      message: "Logged in successfully",
+      data: {
+        user: existingUser,
+        token: token,
+      },
+    });
   } catch (error) {
-        // In case of an error, we can simply send an error message
-        res.status(400).send('user not in db');
+    console.error(error);
+    return res.status(500).send("An error occurred while logging in.");
   }
-}
+};
+
 controller.updateArticle = async (req, res) => {
+
   try {
+    // Find the article by id
+    const article = await Article.findById(req.params.id);
+
+    // Check if the article exists
+    if (!article) {
+      return res.status(404).send({ message: "Article not found" });
+    }
+    if (req.decodedToken.userId !== article.poster.toString()) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    // Check if the article belongs to the user in the JWT token
+
+    // Update the article and return success response
     const updatedArticle = await Article.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true }
     );
-    res.json({ updatedArticle, message: 'updated with successfuly' });
+    res.json({ updatedArticle, message: 'Updated successfully' });
   } catch (error) {
-    res.send(error);
+    console.error(error);
+    return res.status(500).send("An error occurred while updating article.");
   }
 };
-controller.createArticle=async(req,res)=>{
-      const newArticle = new Article(req.body);
-      // Save the new user to the database
-      newArticle.save()
-             .then( article => {
-               // Return success response with the saved user
-               return res.send({
-                 success: true,
-                 message: "Created new article",
-                 data: article
-               });
-             });
-}
+
+controller.createArticle = async (req, res) => {
+
+  try {
+
+    // Create a new article with author id from the JWT token
+    const newArticle = new Article({
+      ...req.body,
+      poster: req.decodedToken.userId
+    });
+
+    // Save the new article to the database
+    const savedArticle = await newArticle.save();
+
+    // Return success response with the saved article
+    res.json({
+      success: true,
+      message: "Created new article",
+      data: savedArticle
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("An error occurred while creating article.");
+  }
+};
 controller.showArticles = async (req, res) => {
   try {
     const allArticle = await Article.find();
@@ -95,20 +178,40 @@ controller.showArticles = async (req, res) => {
 
 controller.myArticles = async (req, res) => {
   try {
-    const allArticle = await Article.find({ poster: req.params.id });
+    // Find all articles posted by the user
+    const allArticle = await Article.find({ poster: req.decodedToken.userId });
     res.json(allArticle);
   } catch (error) {
-    res.status(500).send(error);
+    console.error(error);
+    return res.status(500).send("An error occurred while fetching articles.");
   }
 };
 
-controller.delateArticle = async (req, res) => {    
+
+controller.deleteArticle = async (req, res) => {
+
   try {
+
+    // Find the article by id
+    const article = await Article.findById(req.params.id);
+
+    // Check if the article exists
+    if (!article) {
+      return res.status(404).send({ message: "Article not found" });
+    }
+
+    // Check if the article belongs to the user in the JWT token
+    if (req.decodedToken.userId !== article.poster.toString()) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    // Delete the article and return success response
     await Article.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: 'Article deleted successfully!' });
   } catch (error) {
-    res.status(500).send(error);
+    console.error(error);
+    return res.status(500).send("An error occurred while deleting article.");
   }
 };
 
-module.exports=controller
+module.exports = controller
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const controller = require('../controllers/controller');
-// import all controllers
-// import SessionController from './app/controllers/SessionController';
+const controller = require('../controllers/controller'); // Import all controllers from ../controllers/controller.js
+const User = require('../models/user.model');
 
-const routes = express.Router();
-
+const routes = express.Router(); // Create an instance of Express's Router
 
 
-// // Add routes
-routes.post('/signUp', controller.signUp);
-routes.post('/login', controller.login);
-routes.put('/users/:id', controller.update);//
-/*                article                               */
-routes.post('/articles/new',controller.createArticle);
-routes.get('/articles', controller.showArticles);
-routes.put('/articles/:id', controller.updateArticle);
-routes.delete('/articles/:id', controller.delateArticle);
-routes.post('/users/:id/myArticles', controller.myArticles);
+// Define routes and associate them with the appropriate controller function
+routes.post('/signUp', controller.signUp); // Route to sign up a new user
+routes.post('/login', controller.login); // Route to log in an existing user
+routes.post('/user', controller.verify,async(req,res)=>{
+    
+    const {email,name,mobile}=await User.findById(req.decodedToken.userId)
+    res.send({email,name,mobile})
+}); // Route to log in an existing user
+routes.put('/users', controller.verify, controller.update); // Route to update an existing user (requires authentication)
 
-// routes.post('/', SessionController.store);
-// routes.put('/', SessionController.store);
-// routes.delete('/', SessionController.store);
-
-module.exports = routes;
+/* Routes for managing articles */
+routes.get('/articles', controller.showArticles); // Route to show all articles (requires authentication)
+routes.post('/articles/new', controller.verify, controller.createArticle); // Route to create a new article (requires authentication)
+routes.put('/articles/:id', controller.verify, controller.updateArticle); // Route to update a specific article (requires authentication)
+routes.delete('/articles/:id', controller.verify, controller.deleteArticle); // Route to delete a specific article (requires authentication)
+routes.post('/users/myArticles', controller.verify, controller.myArticles); // Route to show all articles associated with a specific user (requires authentication)
+module.exports = routes; // Export the Router object so it can be used in other files
